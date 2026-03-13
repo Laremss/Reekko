8436c1d87930dbd9868b34a6f7eaf644b42fc12b
Laremss <remi.bitouze@gmail.com>
2026-03-13 11:00:04 +0100

    feat: add Google Tag Manager (GTM-TTB9Z635)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import { headers } from 'next/headers'
+import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
@@ -106,7 +107,7 @@ export default async function RootLayout({
               contactPoint: {
                 '@type': 'ContactPoint',
                 contactType: 'sales',
-                email: 'contact@reekko.com',
+                email: 'contact.reekko@gmail.com',
                 availableLanguage: 'French',
               },
               sameAs: [
@@ -118,9 +119,27 @@ export default async function RootLayout({
         />
       </head>
       <body className="font-sans antialiased bg-zinc-950 text-zinc-50 min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TTB9Z635"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Header />
         <main>{children}</main>
         <Footer />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TTB9Z635');`,
+          }}
+        />
       </body>
     </html>
   )
